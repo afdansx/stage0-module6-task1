530baf0c15637a4efd7cd6609ec60188b365f202
guesswhat <dadwdakwdak@gmail.com>
2024-09-01 11:39:21 +0300

    solved

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,33 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+
+    public String getDescription() {
+
+        String pawsWord = (numberOfPaws == 1) ? "paw" : "paws";
+
+        String furPresence = hasFur ? "a" : "no";
+
+        return String.format("This animal is mostly %s. It has %d %s and %s fur.",
+                color, numberOfPaws, pawsWord, furPresence);
+    }
+
+
+    public static void main(String[] args) {
+
+        Animal myAnimal = new Animal("brown", 4, true);
+
+        System.out.println(myAnimal.getDescription());
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+
+    @Override
+    public String getDescription() {
+
+        String baseDescription = super.getDescription();
+
+        return baseDescription + " Moreover, it has 2 wings and can fly.";
+    }
 }
+
